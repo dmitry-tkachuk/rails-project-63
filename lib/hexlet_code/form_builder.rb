997088eccb9f20ec21b9bbd06b65dd8a71264444
dmitry-tkachuk <dmigtk@gmail.com>
2024-03-17 13:36:00 +0300

    spec fixed

--- lib/hexlet_code/form_builder.rb
+++ lib/hexlet_code/form_builder.rb
@@ -31,9 +31,7 @@ module HexletCode
       @form_body[:submit] = { options: submit_attributes }
     end
 
-    private
-
-    def select_input_class(tag)
+    def self.select_input_class(tag)
       "HexletCode::Inputs::#{tag.capitalize}Input".constantize
     end
   end
